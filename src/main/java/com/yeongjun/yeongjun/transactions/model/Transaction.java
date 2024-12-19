ad0eabbe089362a88de474a75dacdc6dcd3e3475
seo-yeongjun <zanygeek8371@gmail.com>
2024-12-19 16:55:12 +0900

    ..

--- src/main/java/com/yeongjun/yeongjun/transactions/model/Transaction.java
+++ src/main/java/com/yeongjun/yeongjun/transactions/model/Transaction.java
@@ -16,6 +16,8 @@ public class Transaction {
     private Long transaction_id;      // 거래 ID (PK)
     private String username;         // 사용자 이름
     private Long category_id;         // 카테고리 ID
+    private int income_expense_gb;
+    private String category_nm;         // 카테고리 ID
     @Min(value = 1, message = "금액은 1 이상이어야 합니다.")
     private Integer amount;          // 거래 금액
     private Date transaction_date;    // 거래 날짜
